30eff9f83b964816b8a8dc983e281504c7b11aee
pham long <pham duc long>
2019-04-19 00:48:00 +0700

    commit

--- session 8/web_homework2.py
+++ session 8/web_homework2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,
 app = Flask(__name__)
 frofile= [{
         "user":"phamlong1",
@@ -11,23 +11,23 @@ frofile= [{
         "pass":"3"
     }]
 
-@app.route('/',method="POST")
+@app.route('/',method=["POST"])
 def logins():
     user = request.form.get("tai_khoan")
     password = request.form.get("mat_khau")
-    check = 0
+    check = False
     for v in frofile :
         if user == v["user"] and password == v ["pass"] :
-            check == 1 
-        if check == 1:
+            check = True
+    if check == True:
             return render_template("successful.html")
-        else :
+    else :
             return render_template("falied.html")
 
     
-@app.route('/',method="POST")
-def index():
-    return render_template('login.html',data = logins)
+@app.route('/',method=["GET"])
+def get():
+    return render_template('login.html',data = frofile)
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
  
